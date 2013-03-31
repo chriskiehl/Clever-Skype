9f6c3c9b66013f9bedf72ae5584c26de3125b54f
Chris Kiehl <audionautic@gmail.com>
2013-03-30 20:53:40 -0400

    Refactored code. Fixed bug where print statements would overlap due to threading. Changed threads to producer/consumer format.

--- clever_skype_gui.py
+++ clever_skype_gui.py
@@ -1,77 +1,101 @@
+
+"""
+Connects Skype to cleverbot
+
+Note:
+	You'll need to download and install the following before this will run.
+
+	cleverbot:
+	: http://code.google.com/p/pycleverbot/ 
+
+	Skype4Py:
+	: http://sourceforge.net/projects/skype4py/
+
+	WxPython
+	: http://www.wxpython.org/ 
+
+	Alternatively, you could just run the .exe if you're on windows. 
+
+"""
+
+
+
 import wx
-import Skype4Py, cleverbot
-import sys, os, webbrowser, threading
+import Skype4Py
+import cleverbot
+import sys
+import os
+import webbrowser
+import threading
+import Queue
+import time
 from botBuilder import *
 
-"""
-NOTE: 
-
-You'll need to download and install the following before this will run.
-
-cleverbot:
-: http://code.google.com/p/pycleverbot/ 
-
-Skype4Py:
-: http://sourceforge.net/projects/skype4py/
-
-WxPython
-: http://www.wxpython.org/ 
-
-Alternatively, you could just run the .exe if you're on windows. 
-
-"""
 
 class SuperFrame(wx.Frame):
 	def __init__(self):
 		wx.Frame.__init__(self, None, size=(320,390))
-		self.SetTitle("Clever Skype - v.1")
+		self.SetTitle("Clever Skype - v.1.1")
 		icoPath = resource_path('images\\icon.ico')
 		ico = wx.Icon(icoPath, wx.BITMAP_TYPE_ICO)
 		self.SetIcon(ico)
 
 		self.running = False
-
+		font = wx.Font(8, wx.DEFAULT, wx.NORMAL, wx.NORMAL, False, 'Verdana')
 		self.initMenuBar()
+
+		# This is serves as the message pump 
+		# between all the threads. 
+		self.msg_queue = Queue.Queue()
 
 		panel = wx.Panel(self, -1)
 		panel.SetBackgroundColour("white")
 
+		# Used for keping the center column in tact
+		main_hsizer = wx.BoxSizer(wx.HORIZONTAL)
+		main_hsizer.AddStretchSpacer(1)
+
+		# sizer that actually holds all the widgets
 		vsizer = wx.BoxSizer(wx.VERTICAL)
+		vsizer.AddSpacer(12)
 
-		# vsizer.Add(panel,1,wx.EXPAND)
-
-		imgPath = resource_path('images\\clever_skype_logo.png')
-		self.img = wx.Bitmap(imgPath)
-		self.control = wx.StaticBitmap(panel, -1, self.img)
-		vsizer.Add((-1,10))
-		vsizer.Add(self.control, 0, wx.CENTER)
-		vsizer.Add((-1, 16))
-
-		self.statusLabel = wx.StaticText(panel, label="Status")
-		vsizer.Add(self.statusLabel, 0, wx.CENTER)
-		vsizer.Add((-1, 4))
-
-		self.statusBox = wx.TextCtrl(panel, -1, "Click the Start button to begin.", 
-								size=(240,100), style=wx.TE_MULTILINE | wx.TE_READONLY)
-		vsizer.Add(self.statusBox, 0, wx.CENTER)
-		vsizer.Add((0,25))
-
-		hsizer = wx.BoxSizer(wx.HORIZONTAL)
+		self.img = wx.Bitmap('images\\clever_skype_logo.png')
+		self.logo = wx.StaticBitmap(panel, -1, self.img)
+		vsizer.Add(self.logo, 0, wx.ALIGN_CENTER_HORIZONTAL)
+		vsizer.AddSpacer(8)
 		
+		self.statusLabel = wx.StaticText(panel, label="Status")
+		self.statusLabel.SetFont(font)
+		vsizer.Add(self.statusLabel, 0, wx.ALIGN_LEFT | wx.LEFT, 1)
+		vsizer.AddSpacer(5)
+		
+
+		self.statusBox = wx.TextCtrl(panel, -1, 
+							"Click the Start button to begin.", 
+							size=(240,100), 
+							style=wx.TE_MULTILINE | wx.TE_READONLY)
+		vsizer.Add(self.statusBox, 5, wx.CENTER | wx.EXPAND)
+		vsizer.AddSpacer(15)
+		
+		btn_hsizer = wx.BoxSizer(wx.HORIZONTAL)
 		self.startButton = wx.Button(panel, -1, " Start Clever Skype ")
 		self.Bind(wx.EVT_BUTTON, self.onStartButtonDown, self.startButton)
-		hsizer.Add(self.startButton, 0, wx.CENTER)
-		hsizer.Add((20,-1))
-
+		btn_hsizer.Add(self.startButton, 0, wx.ALIGN_LEFT)
+		
+		btn_hsizer.AddStretchSpacer(1)
 		
 		self.stopButton = wx.Button(panel, -1, " Stop Clever Skype ")
 		self.Bind(wx.EVT_BUTTON, self.onStopButtonDown, self.stopButton)
 		self.stopButton.Disable()
-		hsizer.Add(self.stopButton, 0, wx.CENTER)
+		btn_hsizer.Add(self.stopButton, 0, wx.ALIGN_RIGHT)
 
-		vsizer.Add(hsizer, 0, wx.CENTER)
+		vsizer.Add(btn_hsizer, 1, wx.EXPAND)
 
-		panel.SetSizer(vsizer)
+		main_hsizer.Add(vsizer, 0, wx.EXPAND)
+		
+		main_hsizer.AddStretchSpacer(1)
+
+		panel.SetSizer(main_hsizer)
 		# self.Centre()
 		self.Show()
 
@@ -107,7 +131,8 @@ class SuperFrame(wx.Frame):
   		wx.EndBusyCursor() 
 
 	def launchBrowser(self):
-		webbrowser.open('https://github.com/Audionautics/Clever-Skype', new=2) 
+		address = 'https://github.com/Audionautics/Clever-Skype'
+		webbrowser.open(address, new=2) 
 	
 	def onAbout(self, e):
 		description = "Just a silly little toy."
@@ -116,7 +141,7 @@ class SuperFrame(wx.Frame):
 
 		# info.SetIcon(wx.Icon('hunter.png', wx.BITMAP_TYPE_PNG))
 		info.SetName('Clever Skype')
-		info.SetVersion('1.0')
+		info.SetVersion('1.1')
 		info.SetDescription(description)
 		# Need to update to github address
 		info.SetWebSite('https://github.com/Audionautics/')
@@ -125,52 +150,138 @@ class SuperFrame(wx.Frame):
 	def stillRunning(self):
 		return self.running
 
-	def spawn_bot(self, skype, user):
-		robot = SkypeBot(skype, user)
-		while True:
-			if (robot.currentMsgDatetime != None and robot.currentMsgDatetime != robot.getCurrentMessageDatetime()):
-				print 'hello?'
-				self.statusBox.AppendText('\nSkype message from: %s \n' % robot.user.Handle)
-				self.statusBox.AppendText('    contents: "%s"\n\n' % str(robot.getCurrentMessageBody()))
-				
-				robot.update()
-				robot.getCleverResponse()
-				self.statusBox.AppendText('Posting Clever bot\'s respoonse to %s.\n' % robot.user.Handle)
-				self.statusBox.AppendText('Msg: %s.\n' % robot.msg)
-				# print 'getting CleverBot\'s response'
-				# clever_response = cb.Ask(escape_chars(current_msg))
-				# post_skype_msg(s, clever_response)
-				# print 'posting message:', clever_response 
 
-			if not self.stillRunning():
-				break
+	# def spawn_bot(self, skype, user):
+	# 	robot = SkypeBot(skype, user)
+	# 	while True:
+	# 		if not self.newMessageReceived(robot):
+	# 			continue # Wait for user input
 
-	
+	# 		self.statusBox.AppendText('\nSkype message from: %s \n'
+	# 				% robot.user.Handle)
+	# 		self.statusBox.AppendText('    contents: "%s"\n\n' 
+	# 				% str(robot.getCurrentMessageBody()))
+			
+	# 		robot.update()
+	# 		robot.getCleverResponse()
+	# 		self.statusBox.AppendText('Posting Clever bot\'s respoonse to %s.\n' % robot.user.Handle)
+	# 		self.statusBox.AppendText('Msg: %s.\n' % robot.msg)
 
-	def checkUsersOnline(self, skype, onlineList):
+	# 		if not self.stillRunning():
+	# 			break
+
+	# 		time.sleep(.4) 	
+
+	def getUsersOnline(self, skype, usersOnline):
 		for i in skype.Friends:
-			if i.OnlineStatus == 'ONLINE' and i.Handle not in onlineList and i.Handle != "echo123":
-				onlineList.append(i.Handle)
-				threading.Thread(target=self.spawn_bot, args=((skype, i))).start()
+			if (i.OnlineStatus == 'ONLINE' and 
+								i.Handle not in usersOnline and 
+								i.Handle != "echo123"):
+
+				usersOnline.append(i)
 
 	def onStartButtonDown(self, e):
 		self.running = True
 		self.statusBox.Clear()
-		onlineList = []
+
 		skype = Skype4Py.Skype()
 		skype.Attach()
-		for i in skype.Friends:
-			print i.OnlineStatus
-			print i.FullName, i.Handle
-		self.checkUsersOnline(skype, onlineList)
+
+		usersOnline = []
+		self.getUsersOnline(skype, usersOnline)
+
+		
+
+		statusUpdater = StatusUpdater(self.msg_queue, self.postMsg)
+		statusUpdater.setDaemon(True)
+		statusUpdater.start()
+
+		self.bots = []
+		for user in (usersOnline):
+			print 'starting bot for user', user.Handle
+			t = Bot(skype, user, self.msg_queue)
+			t.setDaemon(True)
+			t.start()
+			self.bots.append(t)
+
+
 		self.startButton.Disable()
 		self.stopButton.Enable()
 
 	def onStopButtonDown(self, e):
+		self.statusBox.AppendText('\n\n\n\nDisconnecting CleverSkype.')
 		self.running = False
+		
+		for bot in self.bots:
+			bot._shutdown()
+
+		for bot in self.bots:
+			print bot.is_alive() 
+
 		self.startButton.Enable()
 		self.stopButton.Disable()
+		self.statusBox.AppendText('\nDone.')
 
+	def postMsg(self, msg):
+		self.statusBox.AppendText(msg)
+
+
+class StatusUpdater(threading.Thread):
+	def __init__(self, queue, function):
+		threading.Thread.__init__(self)
+		self.queue = queue
+		self.running = True
+		self.postMsg = function
+
+	def run(self):
+		while self.running:
+			msg = self.queue.get()
+			self.postMsg(msg)
+			
+
+		self.postMsg('Shutting down CleverSkype.')
+
+	def _shutdown(self):
+		self.running = False
+
+class Bot(threading.Thread):
+	Shutdown_queue = None
+	def __init__(self, skype, user, queue):
+		threading.Thread.__init__(self)
+		self.robot = SkypeBot(skype, user)
+		self.queue = queue
+		self.running = True
+
+
+	def newMessageReceived(self):
+		return (self.robot.currentMsgDatetime != None and 
+				self.robot.currentMsgDatetime != 
+				self.robot.getCurrentMessageDatetime())
+
+	def run(self):
+		while self.running:
+			if not self.newMessageReceived():
+				continue # Wait for user input
+
+			self.queue.put('\nSkype message from: %s \n'
+					% self.robot.user.Handle)
+			self.queue.put('    contents: "%s"\n\n' 
+					% str(self.robot.getCurrentMessageBody()))
+			
+			self.robot.update()
+			
+			self.robot.getCleverResponse()
+			self.queue.put('Posting Clever bot\'s respoonse to %s.\n' 
+					% self.robot.user.Handle)
+			
+			self.queue.put('Msg: %s.\n' % self.robot.msg)
+
+			self.queue.task_done()
+
+			time.sleep(.4) 	
+
+	def _shutdown(self):
+		self.running = False
 
 def resource_path(relative):
     return os.path.join(
